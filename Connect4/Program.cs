ad2b97316f4756eb96fd7b09200b4f00782bc526
Troy <Troy@EXCALIBUR>
2017-07-10 00:14:20 -0400

    Added name and debth fields

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using static System.Console;
 
 namespace Connect4
 {
     class Program
     {
+        private static string name;
+        private static Int16 debth;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("A Change");
             int change2 = 0;
         }
+        public static string Name{get{ return name; }
+            set {
+                bool accept = false;
+                while(accept == false)
+                {
+                    if (value.Length > 30 || value.Length < 1)
+                    {
+                        WriteLine("Please enter a name between 1-30 Characters");
+                    }
+                    else
+                        name = value;
+                }
+            } }
     }
 }
