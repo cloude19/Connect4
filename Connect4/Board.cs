8174beaa90642edddc57a62ef274a641486a5cc5
cloude19 <troycloude@gmail.com>
2017-07-11 03:25:32 +0800

    Changed solution name, added Board class and modfied main

---
+++ Connect4/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Connect4
+{
+    /*Create grid array, manage player input into grid, 
+     * Search selected points, get grid state, display grid
+     */
+    class Board
+    {
+        private char[,] Gridd;
+
+        Board(){}
+        Board(int x, int y)
+        {
+            Gridd = new char[x, y];
+            for(int limitx = 0; limitx < x; limitx++)
+            {   for(int limity = 0; limity < y; limity++)
+                {
+                    Gridd[limitx, limity] = '*';
+                }
+
+            }
+            
+        }
+        
+    }
+}
--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -6,13 +6,17 @@ namespace Connect4
     class Program
     {
         private static string name;
-        private static Int16 debth;
+        private static string debth;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("A Change");
-            
+            //get users name and diffculty
+            Console.Write("Enter your Name: ");
+            Name = ReadLine();
+            Console.Write("Enter 1-4 for diffculty: ");
+            Debth = ReadLine();
+
+           
         }
         public static string Name{get{ return name; }
             set {
@@ -22,10 +26,47 @@ namespace Connect4
                     if (value.Length > 30 || value.Length < 1)
                     {
                         WriteLine("Please enter a name between 1-30 Characters");
+                        value = ReadLine();
                     }
                     else
+                    {
                         name = value;
+                        accept = true;
+                    }
                 }
             } }
+        public static string Debth
+        {
+            get
+            {
+                return debth;
+            }
+            set
+            {
+                bool accept = false;
+                while(accept == false)
+                {
+                    if (Int16.TryParse(value, out Int16 test))
+                    {
+                        if (Int16.Parse(value) < 1 || Int16.Parse(value) > 4)
+                        {
+                            WriteLine("Please enter a number between 1-4");
+                            value = ReadLine();
+                        }
+                        else
+                        {
+                            debth = value;
+                            accept = true;
+                        }
+                    }
+                    else
+                    {
+                        WriteLine("Please enter a number value between 1-4");
+                        value = ReadLine();
+                    }
+
+                }
+            }
+        }
     }
 }
